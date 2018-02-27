8a86e48bcad37fd1226c5eb01d22c4fe1b988a50
Joshua Wilkinson <j.d.wilkinson-16@student.lboro.ac.uk>
2018-02-27 00:28:24 +0000

    Added vehicle.html, vehicle.js, initial functionality

---
+++ vehicle.js
@@ -0,0 +1,71 @@
+// Global fake vehicle data
+const fakeVehicleData = [
+    { manufacturer: 'Porsche', model: 'Panamera' },
+    { manufacturer: 'Porsche', model: 'Cayman' },
+    { manufacturer: 'Porsche', model: 'Cayenne' },
+    { manufacturer: 'Porsche', model: 'Macan' },
+    { manufacturer: 'Audi', model: 'R8' },
+    { manufacturer: 'Audi', model: 'S3' },
+    { manufacturer: 'Audi', model: 'RS3' },
+    { manufacturer: 'Honda', model: 'A7' },
+    { manufacturer: 'Renault', model: 'Clio' },
+    { manufacturer: 'Ford', model: 'Focus' }
+];
+
+// Global vehicle component
+Vue.component('vehicle-details', {
+    template: '#template-vehicleDetails',
+    props: ['vehicleID'],
+    data: () => {
+        // Return data object
+        return {
+            vehicleDetails: {
+                manufacturer: '',
+                model: ''
+            },
+            loading: false
+        }
+    },
+    methods: {
+        // Use Lodash's debounce function to prevent updates from happening
+        // too frequently, and execute straight away
+        updateVehicleDetails: _.debounce(function(newVal, oldVal){
+            // Check bounds
+            if(newVal < 0 || newVal > fakeVehicleData.length - 1){
+                // Remove loading flag
+                this.loading = false;
+                
+                return;
+            }
+
+            // New vehicle data
+            var vehicleData = fakeVehicleData[parseInt(newVal)];
+
+            // Set model data
+            this.vehicleDetails = vehicleData;
+
+            // Remove loading flag
+            this.loading = false;
+        }, 600)
+    },
+    watch: {
+        vehicleID: function(newVal, oldVal){
+            // Set loading flag
+            this.loading = true;
+
+            // Call update method
+            this.updateVehicleDetails(newVal, oldVal);
+        }
+    }
+});
+
+// Application component
+var app = new Vue({
+    el: '#container',
+    data: () => {
+        // Return data object
+        return {
+            vehicleIDInput: 0
+        }
+    }
+});
